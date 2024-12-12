5e8e32501ea47d28166be3e0eaad371b7141135f
Allan <gaiteirodeveloper+github@gmail.com>
2024-12-12 20:44:43 -0300

    test de erro

--- src/health-check/health-check.controller.spec.ts
+++ src/health-check/health-check.controller.spec.ts
@@ -3,28 +3,5 @@ import { HealthCheckController } from './health-check.controller';
 import { HealthCheckService } from './health-check.service';
 
 describe('HealthCheckController', () => {
-  let controller: HealthCheckController;
-  let service: HealthCheckService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [HealthCheckController],
-      providers: [HealthCheckService],
-    }).compile();
-
-    controller = module.get<HealthCheckController>(HealthCheckController);
-    service = module.get<HealthCheckService>(HealthCheckService);
-  });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-
-  describe('should return ', () => {
-    it('should return "API is running!www"', () => {
-      expect(controller.getHealthStatus().status).toBe(
-        service.getHealthStatus().status,
-      );
-    });
-  });
 });
