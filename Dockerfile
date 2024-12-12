ec0fcae24234c345a0129768e43c72ae9a5c56d7
Allan <gaiteirodeveloper+github@gmail.com>
2024-12-12 15:48:27 -0300

    base api em nest

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Usar a imagem base do Node.js
+FROM node:18-alpine
+
+# Configurar diretório de trabalho
+WORKDIR /app
+
+# Instalar dependências globais mínimas (opcional, pode instalar no exec)
+RUN npm install -g @nestjs/cli
+
+# Expor a porta onde o app será executado
+EXPOSE 3000
+
+# Definir comando padrão
+CMD [ "sh" ]
---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nestdb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],controllers:[AppController]
+})
+export class AppModule {}
