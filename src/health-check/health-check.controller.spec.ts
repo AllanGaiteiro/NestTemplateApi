a02147464ed975f3f06028fe3a7a898c71c8ec96
Allan <gaiteirodeveloper+github@gmail.com>
2024-12-12 23:27:39 -0300

    teste cov

--- src/health-check/health-check.controller.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { HealthCheckController } from './health-check.controller';
-import { HealthCheckService } from './health-check.service';
-
-describe('HealthCheckController', () => {
-
-});
--- src/health-check/health-check.module.spec.ts
+++
@@ -1,6 +0,0 @@
-import { TestingModule, Test } from '@nestjs/testing';
-import { HealthCheckModule } from './health-check.module';
-
-describe('HealthCheckModule', () => {
-
-});
