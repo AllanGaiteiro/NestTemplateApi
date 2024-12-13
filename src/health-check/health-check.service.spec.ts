56a0a457af8bc791647fc22280618215cce22515
Allan <gaiteirodeveloper+github@gmail.com>
2024-12-12 23:41:06 -0300

    teste

--- src/health-check/health-check.service.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { HealthCheckService } from './health-check.service';
-import { HealthStatusEntity } from './entities/health-status.entity';
-import * as packageJson from '../../package.json';
-
-describe('HealthCheckService', () => {
- 
-});
